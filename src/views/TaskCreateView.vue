495710e02b95d61097e02a728dbcc37f681ec5d9
damitrokhin <damitrokhin@lanit.ru>
2025-02-23 13:28:34 +0300

    correction

--- src/views/TaskCreateView.vue
+++ src/views/TaskCreateView.vue
@@ -8,9 +8,12 @@
         inputHint="Будет отображаться на главной странице"
         placeholder="Введите название"
         v-model="newTaskValue"
-
       />
-      <ZMCheckbox v-model="checkboxStatus" class="checkbox-wraper" label="Является багом" />
+      <ZMCheckbox
+        v-model="checkboxStatus"
+        class="checkbox-wraper"
+        label="Является багом"
+      />
     </div>
     <div class="button-overlay">
       <ZMButton @click="backHome" variant="secondary" label="Вернуться" />
@@ -20,7 +23,7 @@
   </div>
 </template>
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 import ZMCheckbox from "@/components/ZMCheckbox.vue";
 import ZMHeader from "@/components/ZMHeader.vue";
@@ -29,26 +32,23 @@ import ZMButton from "@/components/ZMButton.vue";
 
 const router = useRouter();
 
-const newTaskValue = ref('');
+const newTaskValue = ref("");
 const checkboxStatus = ref(false);
 
-
 const backHome = () => {
-    router.push('/');
+  router.push("/");
 };
 
 const addTask = () => {
-    const currentSavedTasks = [...JSON.parse(localStorage.getItem('tasks'))];
-    currentSavedTasks.push({
-        id: currentSavedTasks.at(-1).id + 1,
-        isBug: checkboxStatus.value,
-        title: newTaskValue.value,
-    });
-    localStorage.setItem('tasks', JSON.stringify(currentSavedTasks));
-    router.push('/');
+  const currentSavedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  currentSavedTasks.push({
+    id: currentSavedTasks.length > 0 ? currentSavedTasks.at(-1).id + 1 : 1,
+    isBug: checkboxStatus.value,
+    title: newTaskValue.value,
+  });
+  localStorage.setItem("tasks", JSON.stringify(currentSavedTasks));
+  router.push("/");
 };
-
-
 </script>
 <style>
 @import "/src/assets/styles/styles.scss";
